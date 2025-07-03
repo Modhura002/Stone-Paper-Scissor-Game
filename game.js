5cffa0d94b89d4bc6bb6ec3336009c888f92e708
Modhura002 <modhura.banerjee@gmail.com>
2025-07-03 17:37:11 +0530

    logic building

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 let button=document.querySelector(".move");
 let boxes=document.querySelectorAll(".choices");
+let playerS=document.querySelector(".my");
+let compS=document.querySelector(".comp");
 
-let playerScore=0;
-let compScore=0;
+let playerScore=1;
+let compScore=1;
 
  const compChoice=()=>{
     const choices=["rock","paper","scissor"];
@@ -17,16 +19,30 @@ let compScore=0;
         let myChoice=box.getAttribute("id");
         console.log("i chose ",myChoice);
         let computerChoice=compChoice();
-        playGame(computerChoice,myChoice);
-
-        
+        playGame(computerChoice,myChoice);  
     });
   });
 
  const playGame=(compChoice,myChoice)=>{
     if(compChoice==myChoice){
-        button.innerText="Its a Tie";
+        button.innerText="Its a Tie! Both chose same";
         button.style.backgroundColor="rgb(16, 15, 30)";
+        
     }
+    else if(myChoice=="rock"){
+        if(compChoice=="scissor"){
+            playerS.innerText=playerScore++;
+            button.innerText="You win! Your rock wins against computer's scissor";
+             button.style.backgroundColor="rgb(7, 142, 23)";
+
+        }
+        else 
+            {compS.innerText=compScore++;
+                button.innerText="You lose! Computer's paper wins against your rock";
+             button.style.backgroundColor="rgb(142, 11, 7)";
+            }
+        
+    }
+    
 
  }
