eec73261ffeccf6377b7306ff06df70ca40f12c1
Modhura002 <modhura.banerjee@gmail.com>
2025-07-03 17:00:02 +0530

    tie condition

--- game.js
+++ game.js
@@ -1,27 +1,30 @@
 let button=document.querySelector(".move");
-let boxes=document.querySelectorAll(".container");
+let boxes=document.querySelectorAll(".choices");
 
 let playerScore=0;
 let compScore=0;
 
  const compChoice=()=>{
     const choices=["rock","paper","scissor"];
+    let random=Math.floor(Math.random()*3);
+    console.log("computer chose" ,choices[random]);
    
-    return choices[Math.floor(Math.random()*3)];
+    return choices[random];
  }
 
   boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
-        let myChoice=box.getAttribute("#id");
-        let compChoice=compChoice();
-        playGame(compChoice,myChoice);
+        let myChoice=box.getAttribute("id");
+        console.log("i chose ",myChoice);
+        let computerChoice=compChoice();
+        playGame(computerChoice,myChoice);
 
         
     });
   });
 
  const playGame=(compChoice,myChoice)=>{
-    if(compChoice===myChoice){
+    if(compChoice==myChoice){
         button.innerText="Its a Tie";
         button.style.backgroundColor="rgb(16, 15, 30)";
     }
