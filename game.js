28f5de8e16a0067c691d8d7fd8f265e3f55df249
Modhura002 <modhura.banerjee@gmail.com>
2025-07-03 16:01:20 +0530

    new logic

--- game.js
+++ game.js
@@ -1,2 +1,8 @@
 let button=document.querySelector(".move");
 let boxes=document.querySelectorAll(".container");
+
+ const compgen=()=>{
+    const choices=["rock","paper","scissor"];
+   
+    return choices[Math.floor(Math.random()) *3];
+ }
